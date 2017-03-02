a117c106869c6647afc676276a3fc4556c2f1694
Manuel Dupont <manuel.dupont@eptica.com>
2017-03-02 11:04:14 +0100

    Create page with conversation area and message input
    - Scroll conversation to bottom when a message is added
    -> Scroll only when app is initialized
    -> Scroll only if conversation is not already at the bottom
    - Send human message when pressing "Enter"

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,13 +5,31 @@
 
     const BOT = {class: "bot"};
     const HUMAN = {class: "human"};
+    var RUNNING = false;
+
+    var scrollConversationToBottom = function() {
+        if (RUNNING) {
+            var conversation = $('#conversation')[0];
+            var height = conversation.scrollHeight;
+            if (conversation.scrollTop < conversation.scrollTopMax) {
+                $(conversation).stop().animate({scrollTop: height}, 500);
+            }
+        }
+    }
+
+    var addMessageToConversation = function(message, role) {
+        if (typeof message !== 'undefined' && message.length > 0) {
+            $('#conversation').append(buildChatPhrase(message, role));
+            scrollConversationToBottom();
+        }
+    }
 
     var sendBotMessage = function(message) {
-        $('#conversation').append(buildChatPhrase(message, BOT));
+        addMessageToConversation(message, BOT);
     };
 
     var sendHumanMessage = function(message) {
-        $('#conversation').append(buildChatPhrase(message, HUMAN));
+        addMessageToConversation(message, HUMAN);
     };
 
     var buildChatPhrase = function(message, role) {
@@ -29,7 +47,22 @@
         return new Date().toLocaleTimeString();
     };
 
+    var sendEnteredMessage = function() {
+        sendHumanMessage($('#message').val());
+        $('#message').val("");
+    }
+
+    $('#send-message').click(sendEnteredMessage);
+    $('#message').keyup(function(event) {
+        // when pressing "Enter"
+        if (event.which === 13) {
+            sendEnteredMessage();
+        }
+    });
+
     sendBotMessage("Hello I'm a bot");
     sendHumanMessage("Hello I'm a human");
+    RUNNING = true;
+    scrollConversationToBottom();
 })();
 
