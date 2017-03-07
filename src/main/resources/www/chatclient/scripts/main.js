3bc9a277d28e3490b7eac2cbd704aa51cde2f7d0
Manuel Dupont <manuel.dupont@eptica.com>
2017-03-07 15:54:27 +0100

    Create page with conversation area and message input
    - Display nb of persons connected
    - Connect to chat server using WebSocket
    -> Handle the different types of messages

--- src/main/resources/www/chatclient/scripts/main.js
+++ src/main/resources/www/chatclient/scripts/main.js
@@ -3,14 +3,17 @@
 (function() {
     var RUNNING = false;
 
+    var socket;
+    var uuid;
     var nickname = localStorage.getItem("nickname");
 
-    var now = function() {
-        return new Date().toLocaleTimeString();
+    var dateTime = function(timestamp) {
+        return new Date(timestamp).toLocaleTimeString();
     };
 
     var setNickname = function(nickname) {
        $('#nickname').text(nickname);
+       socket.send(JSON.stringify({type: 'nickname', nickname: nickname}));
     };
 
     var scrollConversationToBottom = function() {
@@ -24,9 +27,9 @@
     };
 
     var buildChatPhrase = function(message) {
-        var time = $('<div></div>').addClass("col message-time").text("[" + now() + "]");
-        var nick = $('<div></div>').addClass("col message-nickname").text(nickname + ":");
-        var msg = $('<div></div>').addClass("col message-content").text(message);
+        var time = $('<div></div>').addClass("col message-time").text("[" + dateTime(message.timestamp) + "]");
+        var nick = $('<div></div>').addClass("col message-nickname").text(message.from + ":");
+        var msg = $('<div></div>').addClass("col message-content").text(message.text);
 
         return $('<div></div>').addClass("row")
                 .append(time)
@@ -36,8 +39,10 @@
 
     var addMessageToConversation = function(message) {
         if (typeof message !== 'undefined' && message.length > 0) {
-            $('#conversation').append(buildChatPhrase(message));
-            scrollConversationToBottom();
+            socket.send(JSON.stringify({
+                type: "message",
+                text: message
+            }));
         }
     };
 
@@ -64,6 +69,21 @@
         }
     };
 
+    var setNbPersons = function(nbPersons) {
+        $('#nb-personnes').text(nbPersons);
+    };
+
+    var someoneLeft = function(data) {
+        console.log(data.nickname, "left the chat");
+        Materialize.toast('<b>' + data.nickname + '</b>&nbsp;est parti', 4000);
+        setNbPersons(data.nbPersons);
+    };
+
+    var someoneJoined = function(data) {
+        Materialize.toast('<b>' + data.nickname + '</b>&nbsp;a rejoint le chat', 4000);
+        setNbPersons(data.nbPersons);
+    };
+
     $('#user-info-submit').click(updateNickname);
     $('.send-message').click(sendEnteredMessage);
     $('#message').keyup(function(event) {
@@ -74,13 +94,53 @@
     });
     $('.modal').modal();
 
-    if (nickname === null || nickname.length <= 0) {
-        $('#user-info').modal('open');
-    } else {
-        setNickname(nickname);
+
+    // ------------- WebSocket stuff -------------
+
+    socket = new WebSocket("ws://localhost:8581/");
+
+    socket.onopen = function(open){
+        console.log("Socket has been opened with", open.target.url);
+
+        if (nickname === null || nickname.length <= 0) {
+            $('#user-info').modal('open');
+        } else {
+            setNickname(nickname);
+        }
+
+        RUNNING = true;
+        scrollConversationToBottom();
     }
 
-    RUNNING = true;
-    scrollConversationToBottom();
+
+    socket.onmessage = function(msg){
+        console.log("Received message: '" + msg.data + "'");
+        var data = JSON.parse(msg.data);
+        if (data.type === 'welcome') {
+            uuid = data.uuid;
+            setNbPersons(data.nbPersons);
+            console.log("My UUID is " + uuid);
+
+        } else if (data.type === 'newcomer') {
+            someoneJoined(data);
+
+
+        } else if (data.type === 'message') {
+            $('#conversation').append(buildChatPhrase(data));
+            scrollConversationToBottom();
+
+        } else if (data.type === 'leaver') {
+            someoneLeft(data);
+
+        } else {
+            console.log("Unknown message type", data.type);
+        }
+    }
+
+    socket.onclose = function(close) {
+        console.log("Server closed connection");
+    }
+
+    // ------------- WebSocket stuff end -------------
 })();
 
